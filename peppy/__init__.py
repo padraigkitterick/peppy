9233b1f806f2eae3cf134575fae0d428a63e8e2d
Padraig Kitterick <padraigkitterick@gmail.com>
2009-05-13 11:51:45 +0100

    Separated library from script and added the skeleton of a C aes module

---
+++ peppy/__init__.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+"""Implementation of Steve Gibson's "Perfect Paper Passwords" (PPP)
+
+Generates sequences of pseudo-random one-time passwords based on a
+specified passphrase or a 64-character sequence key.
+
+For further information see <http://www.grc.com/ppp.htm>
+
+Copyright (C) 2009 Padraig Kitterick <p.kitterick@psych.york.ac.uk>
+All rights reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+import aes
+
+SHA256_DIGEST_SIZE = 64
+WORD_SIZE = 8 # 1-byte word occupies a maximum of 8 bits
+WORD_COUNT = 128/WORD_SIZE # 16 x 1-byte (8-bit) words = 128 bits
+MAX_WORD_VALUE = (2 ** WORD_SIZE) - 1
+MAX_INT = (2 ** 128) - 1 # maximum possible passcode number
+CHARACTER_ARRAY = "23456789!@#%+=:?abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPRSTUVWXYZ"
+
+def hex2bytes(hexstr):
+    if len(hexstr) is not SHA256_DIGEST_SIZE:
+        return None
+    
+    bytes = []
+    for i in xrange(SHA256_DIGEST_SIZE/2):
+        try:
+            bytes.append(int(hexstr[i*2:(i*2)+2], base=16))
+        except ValueError:
+            return None
+    return bytes
+
+def pack128(n):
+    if not 0 <= n <= MAX_INT:
+        raise IndexError('integer %r cannot be packed into 128 bits.' % hex(n))
+    
+    words = []
+    for i in range(WORD_COUNT):
+        word = n & MAX_WORD_VALUE
+        words.append(int(word))
+        n >>= WORD_SIZE
+    
+    return words
+
+def unpack128(words):
+    n = 0
+    for i, num in enumerate(words):
+        word = num
+        word = word << WORD_SIZE * i
+        n = n | word
+    
+    return n
+
+def create_passcodes(key, chars, startnum, numpass, passlen):
+    num_chars = len(chars)
+    passcode = [" "] * passlen
+    codes = []
+    
+    a = aes.AES()
+    for i in range(startnum, startnum+numpass):
+        val = pack128(i)
+        ciph = a.encrypt(val, key, a.keySize["SIZE_256"])
+        ciph_val = unpack128(ciph)
+        
+        for j in range(passlen):
+            passcode[j] = chars[ciph_val % num_chars]
+            ciph_val = ciph_val / num_chars
+        
+        codes.append(''.join(passcode))
+    return codes
+
+def display_codes(codes, page=None, linelen=7):
+    if len(codes) < linelen:
+        linelen = len(codes)
+    
+    header = "PPP Passcard"
+    line = '-' * ((4*(linelen+1)) + linelen)
+    
+    print header,
+    if page:
+        print ' ' * (len(line)-len(header)-2-len(str(page))),
+        print page,
+    print
+    print line
+    
+    colh = ord('A')
+    print "     ",
+    for i in range(linelen):
+        print " %s  " % chr(colh),
+        colh += 1
+    print
+    
+    col = 0
+    row = 1
+    print "%2d: " % row,
+    for code in codes:
+        if col == linelen:
+            col = 0
+            print
+            row += 1
+            print "%2d: " % row,
+        print code,
+        col += 1
+    print
+    print line
--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 #! /usr/bin/env python
 from distutils.core import setup
 
+classifiers = """\
+Development Status :: 4 - Beta
+Environment :: Console
+Intended Audience :: System Administrators
+License :: OSI Approved :: GNU General Public License (GPL)
+Natural Language :: English
+Programming Language :: Python
+Programming Language :: C
+Topic :: Security
+Topic :: Security :: Cryptography
+Topic :: Utilities
+Operating System :: OS Independent
+
+"""
 setup(name="peppy",
       version="1.0",
+      classifiers = filter(None, classifiers.split("\n")),
       description="Python implementation of Perfect Paper Passwords (PPP)",
       author="Padraig Kitterick",
       author_email="p.kitterick@psych.york.ac.uk",
       url="http://www.padraigkitterick.com/code/ppp/",
       license="GNU GPL",
       packages = ["peppy"],
+      ext_modules=[Extension('peppy._aes',
+                             sources=[os.path.join('peppy', '_aes.c')],
+                             )],
+      scripts=[os.path.join('scripts','peppy')],
       )
